5c1fe85ee0ee45c90191dac59f1c09a13e604273
Maryam Kamel <Maryam.Kamel@linkdev.com>
2021-06-09 22:22:42 +0200

    handling corner case of connection timeout via page dialog service

--- XamarinTask/ViewModels/ArticlesPageViewModel.cs
+++ XamarinTask/ViewModels/ArticlesPageViewModel.cs
@@ -2,6 +2,9 @@
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
+using Prism.Services.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,6 +20,7 @@ namespace XamarinTask.ViewModels
         private HttpClient client;
         private List<Article> articles;
         private INavigationService navigationService;
+        private IPageDialogService dialogService;
         //public
         public string Title
         {
@@ -31,13 +35,19 @@ namespace XamarinTask.ViewModels
         public DelegateCommand ItemSelectedChanged { get; set; }
         public Article SelectdArticle { get; set; }
         //ctor
-        public ArticlesPageViewModel(INavigationService navigationservice)
+        public ArticlesPageViewModel(INavigationService navigationservice,IPageDialogService dialogservice)
         {
             Title = "Articles".ToUpper();
             client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(5);
             navigationService = navigationservice;
+            dialogService = dialogservice;
             ItemSelectedChanged = new DelegateCommand(OnItemSelectedChanged);
         }
+        /// <summary>
+        /// swhn an item gets selected from the articles list
+        /// navigat to article detail page
+        /// </summary>
         private void OnItemSelectedChanged()
         {
             var parameters = new NavigationParameters {
@@ -52,7 +62,7 @@ namespace XamarinTask.ViewModels
 
         public void OnNavigatedTo(INavigationParameters parameters)
         {
-             GetArticlesWebRequest();
+            GetArticlesWebRequest();
         }
 
         private async void GetArticlesWebRequest()
@@ -63,6 +73,10 @@ namespace XamarinTask.ViewModels
                 string content = await requestcontent.Content.ReadAsStringAsync();
                 Root root = JsonConvert.DeserializeObject<Root>(content);
                 Articles = root.articles;
+            }
+            else
+            {
+                await dialogService.DisplayAlertAsync("Alert", "Somthing went wrong, please try again later", "OK");
             }
         }
     }
